669a535d7c97961af7992c3d6958d9e91aa89b36
amin04 <aminmithun1992@gmail.com>
2019-03-15 00:38:59 +0600

    ws chat systme 100% complete

--- ws/server/index.js
+++ ws/server/index.js
@@ -1,13 +1,40 @@
 var server = require('ws').Server;
 var s = new server({ port: 5001 });
+var name;
 
 
 s.on('connection', function(ws) {
-    ws.on('message', function(clientmsg) {
-        console.log("Received:" +clientmsg);
+    ws.on('message', function(message) {
 
-        if(clientmsg == "hello") {
-            ws.send("hello from server");
+        message = JSON.parse(message);
+
+        if(message.type == "name") {
+            ws.personName = message.data;
+            return;
         }
+
+        console.log("Received:" +message);
+
+
+        //all connected client and message
+        s.clients.forEach(function e(client) {
+
+            //not send same massage to client
+            if(client != ws) {
+
+                client.send(JSON.stringify({
+                    name: ws.personName,
+                    data: message.data
+                }));
+
+            }
+        });
+
+        // ws.send(`From server: ${message}`);
     });
+
+    ws.on('close', function(){
+        console.log('one person go to offline');
+    });
+    console.log(`new persion connected`);
 });
